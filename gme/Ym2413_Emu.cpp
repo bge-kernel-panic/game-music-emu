274f012455c35b00259b751d2a12bd93e137b890
Benoit Goudreault-Emond <bgoudreaultemond@gmail.com>
2024-01-07 16:22:52 -0500

    Silly attempt at adding ym2413 support back

--- gme/Ym2413_Emu.cpp
+++ gme/Ym2413_Emu.cpp
@@ -5,17 +5,66 @@
 
 #include "Ym2413_Emu.h"
 
-Ym2413_Emu::Ym2413_Emu() { }
+#include <assert.h>
+// include the C file directly - the correct way to do this would be to link the C file
+// in but I don't know cmake that well :(
+#include "emu2413/emu2413.c"
 
-Ym2413_Emu::~Ym2413_Emu() { }
+Ym2413_Emu::~Ym2413_Emu()
+{
+	if ( opll )
+	{
+		OPLL_delete( opll );
+	}
+}
 
-int Ym2413_Emu::set_rate( double, double ) { return 2; }
+Ym2413_Emu::Ym2413_Emu()
+{
+	opll = 0;
+}
 
-void Ym2413_Emu::reset() { }
+int Ym2413_Emu::set_rate( double sample_rate, double clock_rate )
+{
+	if ( opll )
+	{
+		OPLL_delete( opll );
+		opll = 0;
+	}
+	
+	opll = OPLL_new( clock_rate, sample_rate );
+	if ( !opll )
+		return 1;
+	
+	reset();
+	return 0;
+}
 
-void Ym2413_Emu::write( int, int ) { }
+void Ym2413_Emu::reset()
+{
+	OPLL_reset( opll );
+	OPLL_reset_patch( opll, 0 );
+	OPLL_setMask( opll, 0 );
+	OPLL_set_quality( opll, 0 );
+}
 
-void Ym2413_Emu::mute_voices( int ) { }
+void Ym2413_Emu::write( int addr, int data )
+{
+	OPLL_writeReg( opll, addr, data );
+}
 
-void Ym2413_Emu::run( int, sample_t* ) { }
+void Ym2413_Emu::mute_voices( int mask )
+{
+	OPLL_setMask( opll, mask );
+}
+
+void Ym2413_Emu::run( int pair_count, sample_t* out )
+{
+	while ( pair_count-- )
+	{
+		int s = OPLL_calc( opll );
+		out [0] = s;
+		out [1] = s;
+		out += 2;
+	}
+}
 
--- gme/Ym2413_Emu.h
+++ gme/Ym2413_Emu.h
@@ -5,7 +5,7 @@
 #define YM2413_EMU_H
 
 class Ym2413_Emu  {
-	struct OPLL* opll;
+	OPLL* opll;
 public:
 	Ym2413_Emu();
 	~Ym2413_Emu();
