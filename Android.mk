5eb78acaa86ecd08ece55a301f4334bec1075860
Benoit Goudreault-Emond <bgoudreaultemond@gmail.com>
2024-01-16 19:23:34 -0500

    REALLY fix Android build

--- Android.mk
+++ Android.mk
@@ -102,7 +102,7 @@ LOCAL_SRC_FILES := \
 	gme/Ym2612_Nuked.cpp \
 	gme/Ym2612_GENS.cpp \
 	gme/Ym2612_MAME.cpp \
-        emu2413/emu2413.c \
+        gme/emu2413/emu2413.c \
 	gme/ext/panning.c \
 	gme/gme.cpp
 
